7ec0da3367392906f8bda71331246dd36911e6f6
Sinkov Denis <dsinkov@systematica-consulting.ru>
2023-07-31 12:51:20 +1000

    second commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,11 +2,8 @@ package org.example;
 
 class Test {
     public static class App {
-        public static void main(String[] args) {
-            Object o = ;
-            if (o.equals(true)) {
-                System.out.println("true");
-            }
+        public static void main(String[] args){
+            System.out.println("hello world");
         }
     }
 }
