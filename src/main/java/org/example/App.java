a612dc13d85c8afdae850ac5e9e6456a60e1980e
Esthete9 <712020:3dc02154-6c45-429f-b5ee-4cf59c33afad@bots.bitbucket.org>
2023-08-01 11:04:44 +1000

    удален цикл

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,9 +4,6 @@ class Test {
     public static class App {
         public static void main(String[] args){
             System.out.println("hello world");
-            for (int i = 0; i < 4; i++) {
-                System.out.print("gav!!! ");
-            }
         }
     }
 }
